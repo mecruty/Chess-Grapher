046a4240644c9286f8e25c46f81ad7b832f31321
Terry <terry.wtr.wang@gmail.com>
2026-04-19 20:45:03 -0700

    Jframes now close upon user input

--- src/main/java/io/github/mecruty/Main.java
+++ src/main/java/io/github/mecruty/Main.java
@@ -5,7 +5,8 @@ import picocli.CommandLine;
 
 public class Main {
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new CLI()).execute(args);
-        System.exit(exitCode);
+        //int exitCode = 
+        new CommandLine(new CLI()).execute(args);
+        //System.exit(exitCode);
     }
 }
